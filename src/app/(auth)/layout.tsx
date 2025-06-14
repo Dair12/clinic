adb8eae1fb87e3f45cdd1093ae9aa4203d6a1b2e
Dair12 <dair.aynakulov@gmail.com>
2025-06-14 15:43:22 +0600

    good

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,5 @@
-// app/(auth)/layout.tsx
 import type { ReactNode } from "react";
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
-  );
+  return <>{children}</>;
 }
--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,15 +1,6 @@
 import { isAuthorized } from '@/lib/isAuthorized';
 
-export default async function ProtectedLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  await isAuthorized(); // это можно даже без await — важно, чтобы компонент был async
-
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+export default async function ProtectedLayout({ children }: { children: React.ReactNode }) {
+  await isAuthorized();
+  return <>{children}</>;
 }
