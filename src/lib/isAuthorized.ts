73610fe653a29ffc5f7062415e4dcc8ac396c427
Dair12 <dair.aynakulov@gmail.com>
2025-06-15 18:46:22 +0600

    Update isAuthorized.ts

--- src/lib/isAuthorized.ts
+++ src/lib/isAuthorized.ts
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export async function isAuthorized() {
-  const cookieStore = await cookies(); // cookies() можно вызывать без await
+  const cookieStore = await cookies();
   const token = cookieStore.get('token')?.value;
 
   if (!token) {
